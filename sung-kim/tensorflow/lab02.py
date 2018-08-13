9bc5214c4e06072ce12cce1868597df8245ff974
haenam.jeon <karas1237@sk.com>
2018-08-13 10:55:40 +0900

    init

---
+++ sung-kim/tensorflow/lab02.py
@@ -0,0 +1,23 @@
+# Lab 2 Linear Regression
+import tensorflow as tf
+tf.set_random_seed(777)  # for reproducibility
+
+# X and Y data
+x_train = [1, 2, 3]
+y_train = [1, 2, 3]
+
+# Try to find values for W and b to compute y_data = x_data * W + b
+# We know that W should be 1 and b should be 0
+# But let TensorFlow figure it out
+W = tf.Variable(tf.random_normal([1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+# Our hypothesis XW+b
+hypothesis = x_train * W + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - y_train))
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+print(sess.run(cost))
